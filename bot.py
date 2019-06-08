0c994a106f028d0c63894b55ec4233fa570b3199
souless94 <cl.wk@hotmail.com>
2019-06-08 18:38:25 +0800

    update function for question

--- bot.py
+++ bot.py
@@ -43,6 +43,7 @@ bot = telebot.TeleBot(token = token)
 
 COMMANDS = {'bill':'bill [amount] [service charge] \n gets the bill amount after accounting for gst(7%) and service charge','help':'gets help on the commands','question':'question [name] \n creates a poll','result':'gets results of poll','reset_poll':'resets the poll'}
 history = {}
+sname = "" # name to store
 # handlers
 
 @bot.message_handler(commands = ['bill'])
@@ -77,29 +78,28 @@ def get_answer(message):
     rm = types.InlineKeyboardMarkup()
     UserInput = extract_args(message.text)
     UserInput.extend([""])
-    name = "".join(UserInput)
+    name = " ".join(UserInput)
+    sname = name
     rm.add(types.InlineKeyboardButton("going to be late", callback_data="going to be late"))
     rm.add(types.InlineKeyboardButton("already here", callback_data="already here"))
     rm.add(types.InlineKeyboardButton("on the way", callback_data="on the way"))
     rm.add(types.InlineKeyboardButton("still at home", callback_data="still at home"))
     rm.add(types.InlineKeyboardButton("not coming", callback_data="not coming"))
-    bot.send_message(message.chat.id, '{} where are you?'.format(name), reply_markup=rm)
+    bot.send_message(message.chat.id, '{} where are you?'.format(sname), reply_markup=rm)
     
     
 @bot.callback_query_handler(func=lambda call: call.data in ["going to be late","already here","on the way","still at home","not coming"])
 def test_callback(query):
-    print(query)
     ans = query.data
-    name = query.from_user.first_name
-    history[str(name)] =str(ans)
-    bot.answer_callback_query(query.id , text = "eh i am {} and i am ".format(name) + str(ans))
-    bot.edit_message_text(text= "eh i am {} and i am ".format(name) + str(ans),chat_id =query.message.chat.id ,message_id = query.message.message_id)
+    history[str(sname)] =str(ans)
+    bot.answer_callback_query(query.id , text = "eh i am {} and i am ".format(sname) + str(ans))
+    bot.edit_message_text(text= "eh i am {} and i am ".format(sname) + str(ans),chat_id =query.message.chat.id ,message_id = query.message.message_id)
 
 @bot.message_handler(commands = ['jk'])
 def jk(message):
     jk_photo = open('jun_kai.jpg','rb')
-    jk_photo.close()
     bot.send_photo(message.chat.id,jk_photo)
+    jk_photo.close()
 
 @bot.message_handler(commands = ['result'])
 def display_result(message):
