dfbd5e802b6b74ca83949ac9aa8aded952f04c27
souless94 <cl.wk@hotmail.com>
2019-06-08 19:20:06 +0800

    update code to limit the fun function

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 # helper functions
 
 # def logToFile():
-#     # code reused from labheshr 
+#     # code reused from labheshr
 #     # at https://stackoverflow.com/questions/33711475/python-logging-only-to-file
 #     import logging
 #     logging.basicConfig(filename='bot.log', level=logging.DEBUG,
@@ -17,6 +17,7 @@ import os
 def extract_args(args):
     """a function that takes in a string and then split in spaces and remove the command word"""
     return args.split()[1:]
+
 
 def is_positive_float(arg):
     try:
@@ -30,49 +31,56 @@ def is_positive_float(arg):
 # logging
 # logToFile() # logging that only save to file
 
+
 #######################################
-#server
+# server
 heroku_server = Flask(__name__)
 
 ######################################
 # credentials
 token = creds.token
-bot = telebot.TeleBot(token = token)
+bot = telebot.TeleBot(token=token)
 
 ################################
 
-COMMANDS = {'bill':'bill [amount] [service charge] \n gets the bill amount after accounting for gst(7%) and service charge','help':'gets help on the commands','question':'question [name] \n creates a poll','result':'gets results of poll','reset_poll':'resets the poll'}
+COMMANDS = {'bill': 'bill [amount] [service charge] \n gets the bill amount after accounting for gst(7%) and service charge', 'help': 'gets help on the commands',
+            'question': 'question [name] \n creates a poll', 'result': 'gets results of poll', 'reset_poll': 'resets the poll'}
 history = {}
-sname = "" # name to store
+sname = ""  # name to store
+counter = 0
 # handlers
 
-@bot.message_handler(commands = ['bill'])
+
+@bot.message_handler(commands=['bill'])
 def send_bill(message):
-    gst = 0.07 # 7% gst
-    ans = [0,0] # first input is amount 2nd input is service charge
+    gst = 0.07  # 7% gst
+    ans = [0, 0]  # first input is amount 2nd input is service charge
     userInputs = extract_args(message.text)
-    userInputs.extend([0,0])
+    userInputs.extend([0, 0])
     print(userInputs)
     ans[0] = userInputs[0]
     ans[1] = userInputs[1]
-    response = list(map(is_positive_float,ans))
+    response = list(map(is_positive_float, ans))
     if (False in response):
-        bot.reply_to(message,"invalid input")
+        bot.reply_to(message, "invalid input")
     else:
-        amount = round(float(ans[0]),2)
-        service_charge = round(float(ans[1]),2)
-        finalamount = round(amount * (1+gst+service_charge),2)
-        bot.reply_to(message,"amount is : ${} with a service charge of ${}".format(finalamount,service_charge*amount))
+        amount = round(float(ans[0]), 2)
+        service_charge = round(float(ans[1]), 2)
+        finalamount = round(amount * (1+gst+service_charge), 2)
+        bot.reply_to(message, "amount is : ${} with a service charge of ${}".format(
+            finalamount, service_charge*amount))
 
-@bot.message_handler(commands = ['help'])
+
+@bot.message_handler(commands=['help'])
 def command_help(message):
     help_msg = ""
     for command, desc in COMMANDS.items():
         help_msg += command + " : " + desc + "\n"
         help_msg += "-----------------\n"
-    bot.reply_to(message,"Here are the commands: \n {}".format(help_msg))
- 
-@bot.message_handler(commands = ['question'])
+    bot.reply_to(message, "Here are the commands: \n {}".format(help_msg))
+
+
+@bot.message_handler(commands=['question'])
 def get_answer(message):
     global sname
     # adapted code from https://www.mindk.com/blog/how-to-develop-a-chat-bot/
@@ -81,57 +89,84 @@ def get_answer(message):
     UserInput.extend([""])
     name = " ".join(UserInput)
     sname = name
-    rm.add(types.InlineKeyboardButton("going to be late", callback_data="going to be late"))
-    rm.add(types.InlineKeyboardButton("already here", callback_data="already here"))
+    rm.add(types.InlineKeyboardButton(
+        "going to be late", callback_data="going to be late"))
+    rm.add(types.InlineKeyboardButton(
+        "already here", callback_data="already here"))
     rm.add(types.InlineKeyboardButton("on the way", callback_data="on the way"))
-    rm.add(types.InlineKeyboardButton("still at home", callback_data="still at home"))
+    rm.add(types.InlineKeyboardButton(
+        "still at home", callback_data="still at home"))
     rm.add(types.InlineKeyboardButton("not coming", callback_data="not coming"))
-    bot.send_message(message.chat.id, '{} where are you?'.format(sname), reply_markup=rm)
-    
-    
-@bot.callback_query_handler(func=lambda call: call.data in ["going to be late","already here","on the way","still at home","not coming"])
+    bot.send_message(message.chat.id, '{} where are you?'.format(
+        sname), reply_markup=rm)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in ["going to be late", "already here", "on the way", "still at home", "not coming"])
 def test_callback(query):
     global sname
     ans = query.data
-    history[str(sname)] =str(ans)
-    bot.answer_callback_query(query.id , text = "eh i am {} and i am ".format(sname) + str(ans))
-    bot.edit_message_text(text= "eh i am {} and i am ".format(sname) + str(ans),chat_id =query.message.chat.id ,message_id = query.message.message_id)
+    history[str(sname)] = str(ans)
+    bot.answer_callback_query(
+        query.id, text="eh i am {} and i am ".format(sname) + str(ans))
+    bot.edit_message_text(text="eh i am {} and i am ".format(
+        sname) + str(ans), chat_id=query.message.chat.id, message_id=query.message.message_id)
 
-@bot.message_handler(commands = ['jk'])
+
+@bot.message_handler(commands=['jk'])
 def jk(message):
-    jk_photo = open('jun_kai.png','rb')
-    bot.send_photo(message.chat.id,jk_photo)
-    jk_photo.close()
+    global counter
+    if counter <= 5:
+        counter += 1
+        jk_photo = open('jun_kai.png', 'rb')
+        bot.send_photo(message.chat.id, jk_photo, caption="hi")
+        bot.send_message(message.chat.id, "left : {}".format(counter))
+        jk_photo.close()
+    else:
+        bot.send_message(message.chat.id, "Don't spam ")
 
-@bot.message_handler(commands = ['result'])
+
+@bot.message_handler(commands=['rjk'])
+def reset_jk(message):
+    global counter
+    counter = 0
+    bot.send_message(
+        message.chat.id, "counter reseted \n counter left: {}".format(counter))
+
+
+@bot.message_handler(commands=['result'])
 def display_result(message):
     reply_string = ""
     for name, response in history.items():
-        reply_string += "{} \t {} \n".format(name,response)
+        reply_string += "{} \t {} \n".format(name, response)
     positive_answer = list(history.values())
-    positive = list(filter( lambda x : x == "yes", positive_answer))
-    reply_string += "------------------ \n"+"People replied : {} \n".format(len(history))
-    reply_string += "People agreed : {} | People disagreed : {} \n".format(len(positive),len(history) - len(positive))
+    positive = list(filter(lambda x: x == "yes", positive_answer))
+    reply_string += "------------------ \n" + \
+        "People replied : {} \n".format(len(history))
+    reply_string += "People agreed : {} | People disagreed : {} \n".format(
+        len(positive), len(history) - len(positive))
     bot.send_message(message.chat.id, reply_string)
 
-@bot.message_handler(commands = ['reset_poll'])
+
+@bot.message_handler(commands=['reset_poll'])
 def reset_poll(message):
     history.clear()
-    bot.send_message(message.chat.id,"poll reseted")
-    
+    bot.send_message(message.chat.id, "poll reseted")
+
 ################################
-#server routes
+# server routes
 @heroku_server.route("/"+token, methods=['POST'])
 def get_message():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!",200
+    bot.process_new_updates(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
 
 @heroku_server.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook("https://telebotfin.herokuapp.com/"+token)
-    return "!",200
+    return "!", 200
+
 
 if __name__ == "__main__":
-    heroku_server.run(host="0.0.0.0",port=int(os.environ.get('PORT',5000)))
-
+    heroku_server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
